642977af471a56842f169bfbb64f5af3d8472282
Linda Aluso <68639945+Linda8-lang@users.noreply.github.com>
2025-07-21 11:14:43 -0400

    Add files via upload

---
+++ KPI Queries.sql
@@ -0,0 +1,158 @@
+--KPI Use Cases
+SELECT 
+	dv.VideoName,
+	COUNT(*)AS TotalViews 
+FROM 
+	FactVideoViewing fv
+JOIN 
+	DimVideo dv ON fv.VideoKey=dv.VideoKey
+WHERE 
+	VideoWatched=1
+GROUP BY 
+	VideoName
+--Question 2
+SELECT	
+	Top(10) dv.VideoName,
+	COUNT(*)AS TotalViews 
+FROM 
+	FactVideoViewing fv
+JOIN 
+	DimVideo dv ON fv.VideoKey=dv.VideoKey
+WHERE 
+	VideoWatched=1
+GROUP BY 
+	VideoName
+ORDER BY 
+	TotalViews DESC
+----Question 3
+SELECT 
+	dv.VideoName,
+	AVG(dr.RatingValue) AS AverageRatingValue 
+FROM 
+	FactVideoViewing fv
+JOIN 
+	DimVideo dv ON fv.VideoKey=dv.VideoKey 
+JOIN 
+	DimRating dr ON fv.RatingKey=dr.RatingKey
+WHERE 
+	VideoWatched=1
+GROUP BY 
+	VideoName
+ORDER BY 
+	VideoName
+--- Question 4
+SELECT 
+	dv.VideoName,
+	dv.VideoID 
+FROM 
+	FactVideoViewing fv
+JOIN 
+	DimVideo dv ON fv.VideoKey=dv.VideoKey
+WHERE 
+	VideoWatched=0 AND TrailerWatched=1
+GROUP BY 
+	VideoName,VideoID
+---Question 5
+SELECT 
+	dv.VideoName,
+	dv.VideoID,
+	COUNT(fv.VideoKey) AS TotalViews 
+FROM 
+	FactVideoViewing fv
+JOIN 
+	DimVideo dv ON fv.VideoKey=dv.VideoKey
+JOIN 
+	BridgevideoCast bv ON dv.VideoKey=bv.VideoKey
+JOIN 
+	DimCast dc ON bv.CastKey=dc.CastKey
+WHERE 
+	CastName='Dwayne John'
+GROUP BY 
+	VideoName, VideoID
+
+
+--SELECT *FROM BridgevideoCast
+-------- Trend of Video viewing since release date
+SELECT 
+	dv.VideoName,
+	dv.VideoID,
+	dd.ViewingDate,
+	dv.ReleaseYear,
+	DATEDIFF(DAY,dv.ReleaseYear,dd.ViewingDate) AS DaysSinceRelease,
+	COUNT(fv.VideoKey) AS TotalViewsForDay
+FROM 
+	FactVideoViewing fv 
+JOIN 
+	DimVideo dv ON fv.VideoKey=dv.VideoKey 
+JOIN 
+	DimDate dd ON fv.DateKey=dd.DateKey
+WHERE 
+	VideoWatched=1 -----Count actual views only
+AND 
+	ReleaseYear IS NOT NULL-----Ensures release date exist
+GROUP BY
+	dv.VideoName,
+	dv.VideoID,
+	dd.ViewingDate,
+	dv.ReleaseYear,
+	DATEDIFF(DAY,dd.ViewingDate,dv.ReleaseYear)
+----Grouping (Binning/Categories) users by age for video recommendation
+SELECT 
+	CASE
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 0 AND 24 THEN '0-24'
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 25 AND 34 THEN '25-34'
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 35 AND 44 THEN '35-44'
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 45 AND 54 THEN '45-54'
+		ELSE '55+'
+	END AS AgeGroup,
+	COUNT(dc.CustomerKey) AS NumberOfCustomers
+
+FROM
+	DimCustomer dc
+WHERE 
+	DateOfBirth IS NOT NULL----Ensures that the date of birth exists.
+GROUP BY 
+	CASE
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 0 AND 24 THEN '0-24' -----GetDATE() fetch the current date
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 25 AND 34 THEN '25-34'
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 35 AND 44 THEN '35-44'
+		WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 45 AND 54 THEN '45-54'
+		ELSE '55+'
+	END	
+ORDER BY
+	AgeGroup;
+-----Use Common Table Expressions (CTEs) to avoid aliases syntax error and to separate transformations and aggregation
+WITH c AS(
+	SELECT 
+		dc.CustomerKey,------------Keep the key for later join
+		CASE
+			WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 0 AND 24 THEN '0-24'
+			WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 25 AND 34 THEN '25-34'
+			WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 35 AND 44 THEN '35-44'
+			WHEN DATEDIFF(YEAR,dc.DateOfBirth,GETDATE()) BETWEEN 45 AND 54 THEN '45-54'
+			ELSE '55+'
+	END AS AgeGroup
+	FROM
+		DimCustomer dc
+	WHERE dc.DateOfBirth IS NOT NULL)
+----Transformation and aggregation
+SELECT 
+	AgeGroup,
+	dv.VideoName,
+	COUNT(fv.ViewingKey) AS TotalViewing,
+	COUNT(*) AS NumberOfCustomers
+FROM 
+	c
+JOIN
+	FactVideoViewing fv ON fv.CustomerKey=c.CustomerKey
+
+JOIN
+	DimVideo dv ON fv.VideoKey=dv.VideoKey
+WHERE fv.VideoWatched=1
+GROUP BY
+	c.AgeGroup,dv.VideoName
+ORDER BY
+	AgeGroup,dv.VideoName
+
+
+		
---
+++ Table Schema.sql
@@ -0,0 +1,70 @@
+USE MSBA56
+----First dimensional table
+CREATE TABLE DimCustomer(
+CustomerKey INT PRIMARY KEY NOT NULL,
+CustomerID VARCHAR(24) NOT NULL,
+CustomerName VARCHAR(24),
+Email VARCHAR(24),
+DateOfBirth VARCHAR(24),
+MembershipStatus VARCHAR(24))
+----Second dimensional table
+CREATE TABLE DimTime(
+TimeKey INT PRIMARY KEY NOT NULL,
+TimeValue TIMESTAMP,
+TimeOfDayCategory VARCHAR(24))
+-----Third dimensional table
+CREATE TABLE DimDate(
+DateKey INT PRIMARY KEY NOT NULL,
+ViewingDate DATE,
+DayName VARCHAR(24))
+---- Fourth dimensional table
+CREATE TABLE DimRating(
+RatingKey INT PRIMARY KEY NOT NULL,
+RatingID VARCHAR(24) NOT NULL,
+RatingValue int,
+RatingDescription VARCHAR(64))
+--DROP TABLE DimRating
+------Fifth dimensional tab.le
+CREATE TABLE DimVideo(
+VideoKey INT PRIMARY KEY NOT NULL,
+VideoID VARCHAR(24) NOT NULL,
+VideoName VARCHAR(64),
+RunTimeMinutes INT,
+Genre VARCHAR(24),
+ReleaseYear DATE NOT NULL,
+Director VARCHAR(24),
+Producer VARCHAR(24))
+----Sixth dimensional table
+CREATE TABLE DimCast(
+CastKey INT PRIMARY KEY NOT NULL,
+CastID VARCHAR(24) NOT NULL,
+CastName VARCHAR(64),
+Role VARCHAR(24))
+----Bridge to break many to many relationship
+CREATE TABLE BridgevideoCast(
+CastKey INT,
+VideoKey INT,
+FOREIGN KEY (CastKey) REFERENCES DimCast(CastKey),
+FOREIGN KEY (VideoKey) REFERENCES DimVideo(VideoKey))
+----Fact Table
+CREATE TABLE FactVideoViewing(
+ViewingKey INT PRIMARY KEY IDENTITY(1,1),
+VideoKey INT,
+CustomerKey INT,
+RatingKey INT,
+DateKey INT,
+TimeKey INT,
+ViewingStartTime DATETIME,
+ViewingEndTime DATETIME,
+VideoWatched INT,
+TrailerWatched INT,
+VideoDownloaded INT,
+PercentageWatched INT,
+FOREIGN KEY (VideoKey) REFERENCES DimVideo(VideoKey),
+FOREIGN KEY (CustomerKey) REFERENCES DimCustomer(CustomerKey),
+FOREIGN KEY (RatingKey) REFERENCES DimRating(RatingKey),
+FOREIGN KEY (DateKey) REFERENCES DimDate(DateKey),
+FOREIGN KEY (TimeKey) REFERENCES DimTime(TimeKey)
+)
+--DROP TABLE FactVideoViewing
+SELECT * FROM FactVideoViewing
